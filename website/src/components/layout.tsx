ed633635181c6b88fcf79507deab10bf6cbc9eb1
smp46 <me@smp46.me>
2025-06-28 17:37:50 +1000

    Log viewer added

--- website/src/components/layout.tsx
+++ website/src/components/layout.tsx
@@ -16,7 +16,7 @@ interface LayoutProps {
 
 export default function RootLayout({ children }: LayoutProps) {
   return (
-    <div className="flex h-screen w-full overflow-hidden">
+    <div className="flex min-h-screen w-full">
       <Head>
         <meta httpEquiv="content-language" content="en-us"></meta>
         <meta name="author" content="The Honeypotters"></meta>
