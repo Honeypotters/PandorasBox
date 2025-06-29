38dcdb9a12817470b310719201fc9666f7efa3b2
Bangu <Bangu7573@gmail.com>
2025-06-29 20:24:30 +1000

    Updated model training and usage. Added file syntheticifier which generates synthetic data.

--- aillm/model/train_model.py
+++ aillm/model/train_model.py
@@ -1,4 +1,4 @@
-import numpy as np
+import argparse
 from datasets import load_dataset
 from transformers import (
     AutoModelForCausalLM,
@@ -7,6 +7,20 @@ from transformers import (
     Trainer
 )
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Fine-tuning GPT2 model")
+
+    parser.add_argument(
+        "--run_name",
+        type=str,
+        required=True,
+        help="Name of the current run",
+    )
+    args = parser.parse_args()
+    return args
+
+args = parse_args()
+
 CHECKPOINT = "distilgpt2"
 DATA_FP = "data/flows_formatted.jsonl"
 SAVE_LOCATION = "model/saved_model/run_1"
@@ -14,13 +28,14 @@ SAVE_LOCATION = "model/saved_model/run_1"
 def tokenize_data():
     tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
     tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.add_special_tokens({"eos_token": "<END>"})
 
     dataset = load_dataset("json", data_files=DATA_FP, split="train")
 
     def format(example):
         prompt = example["prompt"]
         response = example["response"]
-        full_text = f"<PROMPT>\n{prompt}\n\n<RESPONSE>\n{response}"
+        full_text = f"<PROMPT>\n{prompt}\n\n<RESPONSE>\n{response}<END>"
         tokens = tokenizer(
             full_text,
             truncation=True,
@@ -36,6 +51,7 @@ def tokenize_data():
 def training(data, tokenizer):
     model = AutoModelForCausalLM.from_pretrained(CHECKPOINT)
 
+    # Hyperparameters
     training_args = TrainingArguments(
         # "test-trainer",
         evaluation_strategy="epoch",
@@ -56,6 +72,7 @@ def training(data, tokenizer):
         # lr_scheduler_type="cosine",
     )
 
+    # Actual training
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -70,6 +87,8 @@ def training(data, tokenizer):
 def main():
     tokenized_dataset, tokenizer = tokenize_data()
     trainer = training(data=tokenized_dataset, tokenizer=tokenizer)
+
+    # Save the trained model and tokenizer
     trainer.save_model(SAVE_LOCATION)
     tokenizer.save_pretrained(SAVE_LOCATION)
 
